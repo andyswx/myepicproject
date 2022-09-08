ff68fbb8fb1dd9750d8285925e25b1aed7c4af40
Andrew Szwec <andrewszwec@Andrews-MacBook-Pro.local>
2022-09-08 20:31:30 +0200

    first commit

---
+++ programs/myepicproject/src/lib_votes.rs
@@ -0,0 +1,68 @@
+use anchor_lang::prelude::*;
+
+declare_id!("DNobpQVd2eyGfUq8LwQtX7KbWZEeNCQoFTqQSBxeqWQY");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+
+  // The function now accepts a gif_link param from the user. We also reference the user from the Context
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+    let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+	// Build the struct.
+    let item = ItemStruct {
+      gif_link: gif_link.to_string(),
+      user_address: *user.to_account_info().key,
+      votes: 0
+    };
+		
+	// Add it to the gif_list vector.
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
+  pub fn update_item(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+  
+  }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub votes: u64,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+	// Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
